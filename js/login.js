be7e826f92db37df4105a726dfb63521905bca21
tamadNaBrowNie <77228447+tamadNaBrowNie@users.noreply.github.com>
2021-12-07 14:14:08 +0800

    fixed the submit logic. changed to alert instead of console for more visible debbugging

--- js/login.js
+++ js/login.js
@@ -18,19 +18,19 @@
 }) 
 */
 $(function () {
-    $("#login").click();
-    $(".form-Reg").submit(function () {
-            
-        var User = {
-            Name:String, Password:String
-        }
     
-        User.Name = $("#tName").val();
-        User.Password = $("#pPassword").val();
-        var sUser = JSON.stringify(User);
-    
-        console.log(sUser);
-            
-    })
+    $(".reg-Form").submit(() => {
+
+            var User = {
+                Name: String, Password: String
+            };
+
+            User.Name = $("#tName").val();
+            User.Password = $("#pPassword").val();
+            var sUser = JSON.stringify(User);
+
+            alert(sUser);
+
+        })
 
 });
