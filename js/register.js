414efa35a96176b0b64b27e940ec85bb8fd7455c
tamadNaBrowNie <77228447+tamadNaBrowNie@users.noreply.github.com>
2021-12-08 02:51:34 +0800

    fixed the jquery, had to remove the logic that closes the form when someone clicks outside it. I still have not ported it to jq properly yet

--- js/register.js
+++ js/register.js
@@ -1,7 +1,11 @@
 $(() => {//this is short-shorthand for $(document).ready(function() {})
+    $('#getRegistered').click(()=>{ $('#regDiv').show();})
+    $('.cancel, .close').click(() => { $('#regDiv').hide(); });
     
-    $('body').on('click', '#getRegistered',function () {
-        $('#regDiv').css('display', 'block');        
-    });
-    $('.cancel, #regDiv,.close').click(()=>{ $('#regDiv').hide(); });
+    $('body').on('click', '.modal', () => {$('#regDiv').hide();});
+    $('.modal').click((modal) => {
+        if (modal.target !== this) {
+            $('#regDiv').hide();
+        }
+    })
 });
