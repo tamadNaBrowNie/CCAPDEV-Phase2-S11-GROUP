bcbb18934cd00cbe35e24a8cc763d10ce40478b4
tamadNaBrowNie <77228447+tamadNaBrowNie@users.noreply.github.com>
2022-02-11 20:13:55 +0800

    did somethigns to register. it also installed handlebars

--- PROJECT-w-handlebars/public/js/register.js
+++ PROJECT-w-handlebars/public/js/register.js
@@ -10,6 +10,11 @@ $(() => {//this is short-shorthand for $(document).ready(function() {})
             $('.modal').hide();
         }
     });
+    $('[name = "submit"]').click(() => {
+        User.Name = $('#userName').val();
+        User.Password = $('#passWord').val();
+        $.post('localhost:3000', User).done((res) => {"you are now registered" + res})
+    })
 /*TODO
     if we need extra logic for none http things, this is where we put it.
 */
