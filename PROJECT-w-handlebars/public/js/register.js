e0238e973cc2b72e53af0d55a959a5e302769b12
tamadNaBrowNie <77228447+tamadNaBrowNie@users.noreply.github.com>
2022-02-17 15:04:27 +0800

    jquery works now I guess

--- PROJECT-w-handlebars/public/js/register.js
+++ PROJECT-w-handlebars/public/js/register.js
@@ -6,7 +6,7 @@ $(() => {//this is short-shorthand for $(document).ready(function() {})
     };
 
   
-    $('#getRegistered').click(()=>{ $('#regDiv').show();})
+    $('#getRegistered').click(() => { $('#regDiv').show(); })
     $('.cancel, .close').click(() => { $('#regDiv').hide(); });
     $('.modal').click((modal) => {
         if ($(modal.target).is('.modal')) {
@@ -17,10 +17,19 @@ $(() => {//this is short-shorthand for $(document).ready(function() {})
         User.Name = $('#userName').val();
         User.Password = $('#passWord').val();
         User.Email = $('#eMail').val();
-$.post('localhost:3000/submit-user', User, 'json');
-    })
+        
 
-/*TODO
-    if we need extra logic for none http things, this is where we put it.
-*/
-});
+        /*TODO
+            if we need extra logic for none http things, this is where we put it.
+        */
+    });
+    $.ajax({
+        type: "POST",
+        url: "/submit-user",
+        data: User,
+        dataType: 'json',
+        success: function (response) {
+            console.log(response);
+        }
+    });
+})
